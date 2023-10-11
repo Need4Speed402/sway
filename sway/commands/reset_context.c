255241609a8a47c57a23141010206dc39686cfa0
Alexander Orzechowski <alex@ozal.ski>
2023-10-10 22:31:50 -0400

    Introduce reset_context to simulate a reset

---
+++ sway/commands/reset_context.c
@@ -0,0 +1,13 @@
+#include <assert.h>
+#include <stdlib.h>
+#include <strings.h>
+#include "sway/commands.h"
+#include "sway/tree/view.h"
+#include "log.h"
+
+struct cmd_results *cmd_reset_context(int argc, char **argv) {
+	struct wlr_output_manager *manager = &server.output_manager;
+	wl_signal_emit_mutable(&manager->primary.renderer->events.lost, NULL);
+
+	return cmd_results_new(CMD_SUCCESS, NULL);
+}
